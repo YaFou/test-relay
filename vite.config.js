1b7d0c315747902ea33e4690df02e1b66696986b
YaFou <33806646+YaFou@users.noreply.github.com>
2021-06-22 19:55:33 +0200

    add esbuild plugin

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,41 @@
 import {defineConfig} from "vite";
 import reactRefresh from '@vitejs/plugin-react-refresh'
 
+import { promises } from "fs";
+import crypto from "crypto";
+import path from "path";
+import { print, parse } from "graphql";
+
+const plugin = {
+    name: "relay",
+    setup: build => {
+        build.onLoad({ filter: /\.tsx$/, namespace: "" }, async args => {
+            let contents = await promises.readFile(args.path, "utf8");
+
+            if (contents.includes("graphql`")) {
+                let imports = [];
+
+                contents = contents.replaceAll(/graphql`([\s\S]*?)`/gm, (match, query) => {
+                    const formatted = print(parse(query));
+                    const name = /(fragment|mutation|query) (\w+)/.exec(formatted)[2];
+                    // const hash = crypto.createHash("md5").update(formatted, "utf8").digest("hex");
+
+                    let id = `graphql__${crypto.randomBytes(10).toString("hex")}`;
+                    imports.push(`import ${id} from "./__generated__/${name}.graphql.ts";`);
+                    return id;
+                });
+
+                contents = imports.join("\n") + contents;
+            }
+
+            return {
+                contents: contents,
+                loader: "tsx",
+            };
+        });
+    },
+};
+
 export default defineConfig({
     define: {
         global: 'window',
@@ -11,5 +46,12 @@ export default defineConfig({
     ],
     esbuild: {
         jsxInject: 'import React from "react";'
+    },
+    optimizeDeps: {
+        esbuildOptions: {
+            plugins: [
+                plugin
+            ]
+        }
     }
 })
